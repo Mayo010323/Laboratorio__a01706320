c2593d8676d05630da57360b6a56214072ca9845
Mayo010323 <113480858+Mayo010323@users.noreply.github.com>
2023-11-13 19:37:44 -0600

    Update Prueba.py

--- Prueba.py
+++ Prueba.py
@@ -1,23 +1,28 @@
-# Draw a title and some text to the app:
-'''
-# This is the document title
-
-This is some _markdown_.
-'''
-
+import random
 import pandas as pd
-df = pd.DataFrame({'col1': [1,2,3]})
-df  # 👈 Draw the dataframe
+import streamlit as st
 
-x = 10
-'x', x  # 👈 Draw the string 'x' and then the value of x
-
-# Also works with most supported chart types
-import matplotlib.pyplot as plt
-import numpy as np
-
-arr = np.random.normal(1, 1, size=100)
-fig, ax = plt.subplots()
-ax.hist(arr, bins=20)
-
-fig  # 👈 Draw a Matplotlib chart
+df = pd.DataFrame(
+    {
+        "name": ["Roadmap", "Extras", "Issues"],
+        "url": ["https://roadmap.streamlit.app", "https://extras.streamlit.app", "https://issues.streamlit.app"],
+        "stars": [random.randint(0, 1000) for _ in range(3)],
+        "views_history": [[random.randint(0, 5000) for _ in range(30)] for _ in range(3)],
+    }
+)
+st.dataframe(
+    df,
+    column_config={
+        "name": "App name",
+        "stars": st.column_config.NumberColumn(
+            "Github Stars",
+            help="Number of stars on GitHub",
+            format="%d ⭐",
+        ),
+        "url": st.column_config.LinkColumn("App URL"),
+        "views_history": st.column_config.LineChartColumn(
+            "Views (past 30 days)", y_min=0, y_max=5000
+        ),
+    },
+    hide_index=True,
+)
