27a0f38e6161f3fd3c0e3898a17f4fbd08468197
Mayo010323 <113480858+Mayo010323@users.noreply.github.com>
2023-11-13 19:30:51 -0600

    Update Prueba.py

--- Prueba.py
+++ Prueba.py
@@ -1,26 +1,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
 
-# Generar datos de ejemplo
-data = pd.DataFrame({
-    'x': np.arange(100),
-    'y': np.random.randn(100)
-})
+df = pd.DataFrame(np.random.randn(50, 20), columns=("col %d" % i for i in range(20)))
 
-# Crear una aplicación Streamlit simple
-st.title('Gráfico con Streamlit')
-
-# Agregar una gráfica al cuerpo de la aplicación
-st.line_chart(data.set_index('x'))
-
-# Puedes personalizar la gráfica según tus necesidades
-# Por ejemplo, agregar un título y etiquetas de ejes
-plt.plot(data['x'], data['y'])
-plt.title('Gráfico con Matplotlib')
-plt.xlabel('Eje X')
-plt.ylabel('Eje Y')
-
-# Mostrar la gráfica en Streamlit
-st.pyplot(plt)
+st.dataframe(df)  # Same as st.write(df)
