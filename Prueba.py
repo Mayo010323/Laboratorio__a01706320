fb88acc4dae2ec71f070523a9ee2d7a94ccd86e4
Mayo010323 <113480858+Mayo010323@users.noreply.github.com>
2023-11-15 19:35:11 -0600

    Update Prueba.py

--- Prueba.py
+++ Prueba.py
@@ -7,3 +7,16 @@ from PIL import Image
 st.title("Titulo: Analitica de datos")
 st.header("Este es un header")
 st.subheader("Este es un subheader")
+
+
+st.text("Texto: Hola stream")
+st.markdown("#Este es un markdown h1 \n ## Este es un h2 \n ### Este es un h3")
+st.header("Successful")
+st.info("Información")
+st.warning("warning")
+st.error("Error")
+st.exception("NameError('no está definido')")
+st.header("Obtener informacion de ayuda python")
+st.help(range)
+st.header("widgets: ")
+st.subheader("Checkbox")
